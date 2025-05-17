cb9f63e72a84affcac81dbfb113fd3aec5670d92
xJasonShane <jenson_shan@yeah.net>
2025-05-17 12:34:53 +0800

    feat: 添加绘制矩形的运动逻辑
    添加了绘制矩形的运动逻辑，包括向前移动、左转和右转功能，以实现在Godot中绘制矩形的基本功能。

---
+++ 004-Draw a rectangle.gd
@@ -0,0 +1,22 @@
+// 实体运动200个像素位置
+func run():
+    move_forward(200)
+
+// 实体向左和向右旋转度数 独立函数单独在Godot中无法执行turn_right()和turn_left()
+func run():
+    move_forward(80)
+    turn_right(45)
+    move_forward(80)
+    turn_left(45)
+    move_forward(80)
+
+
+func run():
+    move_forward(200)
+    turn_right(90)
+    move_forward(120)
+    turn_right(90)
+    move_forward(200)
+    turn_right(90)
+    move_forward(120)
+    turn_right(90)
